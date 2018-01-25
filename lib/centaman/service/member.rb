49df1578b4dc33a7d0d47bc48f56e8d68404de14
dianacamacho <dcam1901@gmail.com>
2018-01-25 16:45:52 -0600

    add ability to find member by email in member service

--- lib/centaman/service/member.rb
+++ lib/centaman/service/member.rb
@@ -2,14 +2,16 @@ module Centaman
   class Service::Member < Centaman::Service
     include Centaman::JsonWrapper
 
-    attr_reader :member_code
+    attr_reader :member_code, :email
 
     def after_init(args)
       @member_code = args.fetch(:member_code, nil).try(:to_i)
+      @email = args.fetch(:email, nil)
+      required_args
     end
 
     def endpoint
-      "/member_services/Member/#{member_code}"
+      member_code ? member_endpoint(:member_code) : member_endpoint(:email)
     end
 
     def object_class
@@ -29,6 +31,27 @@ module Centaman
     end
 
     private
+
+    def member_endpoint(attr_key)
+      attr_val = self.send(attr_key)
+      endpoint_url(attr_key, attr_val)
+    end
+
+    def endpoint_url(key, val)
+      endpoint_options = {
+        member_code: "/member_services/Member/#{val}",
+        email: "/member_services/Member?email=#{val}"
+      }
+      endpoint_options[key]
+    end
+
+    def args_missing
+      arg_missing = [member_code, email].compact.empty?
+    end
+
+    def required_args
+      raise "missing required arguments. #{self.class} must be instantiated with a :member_code or :email" if args_missing
+    end
 
     def not_found(resp)
       { error: resp.parsed_response ? resp.parsed_response : 'Member not found' }
