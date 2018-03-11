c7dbe4a86ffe3962b2ecf24a0c70dabff813ee5a
dianacamacho <dcam1901@gmail.com>
2018-03-11 04:09:21 -0500

    renewal ip, checks for existing memberships to set mem renewal

--- lib/centaman/service/purchase_membership.rb
+++ lib/centaman/service/purchase_membership.rb
@@ -2,18 +2,17 @@ module Centaman
   class Service::PurchaseMembership < Centaman::Service
     attr_reader :payment_reference, :order_info, :checkout_service, :coupon_service,
                 :members, :memberships, :add_ons, :membership_type_id,
-                :is_new, :purchaser_renewal, :is_new, :coupon, :has_card_discount
+                :is_new, :is_new, :coupon, :has_card_discount
 
     def after_init(args)
       @payment_reference = args[:payment_reference]
       @order_info = args[:order_info]
-      @purchaser_renewal = @order_info.is_renewal
       @is_new = !@order_info.is_renewal
       @has_card_discount = @order_info.fnbo_discount
       @checkout_service = args[:checkout_service]
       @coupon_service = args[:coupon_service]
       @membership_type_id = @order_info.membership_type_id
-      @members = @order_info.members
+      @members = @order_info.update_members
       @add_ons = @checkout_service.add_ons
       @memberships = []
       @coupon = @coupon_service.valid_coupon
@@ -21,26 +20,27 @@ module Centaman
     end
 
     def endpoint
+      p "/member_services/Membership?isNew=#{is_new}"
       "/member_services/Membership?isNew=#{is_new}"
     end
 
-    def membership_payload(member_id, add_on)
+    def membership_payload(member, add_on)
       payload = {
-        'MemberCode': member_id,
+        'MemberCode': member.id,
         'TypeCode': add_on.id,
         'Cost': add_on.cost,
         'Tax': checkout_service.add_on_tax(add_on),
         'Paid': add_on.pay_price,
         'PackageID': membership_type_id,
-        'PurchaserRenewal': purchaser_renewal,
+        'PurchaserRenewal': member.memberships.any?,
         'PaymentGatewayReference': payment_reference
       }
       payload = coupon.present? ? payload.merge(coupon_args(add_on)) : payload
-      payload = has_card_discount ? payload.merge(card_discount_args) : payload
+      payload = has_card_discount ? payload.merge(card_discount_args(add_on)) : payload
     end
 
     def coupon_args(add_on)
-      return empty_coupon_args unless coupon_service.coupon_applies(add_on)
+      return {} unless coupon_service.coupon_applies(add_on)
       {
         'Coupon': {
           "CouponCode": coupon.code,
@@ -50,8 +50,8 @@ module Centaman
       }
     end
 
-    def card_discount_args
-      return empty_card_args unless has_card_discount
+    def card_discount_args(add_on)
+      return {} unless add_on.add_on_type == 'primary'
       {
         'CardDiscount': {
           'DiscountAmount': checkout_service.fnbo_amount,
@@ -81,8 +81,9 @@ module Centaman
 
     def build_membership_request
       members.map do |m|
+        # binding.pry
         order_info.add_ons_for_member(add_ons, m.member_type).each do |ao|
-          @memberships << membership_payload(m.id, ao)
+          @memberships << membership_payload(m, ao)
         end
       end
     end
