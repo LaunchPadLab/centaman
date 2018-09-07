6f4f9fd76b1dbde1b583612272de9b735f7cd5c0
dianacamacho <dcam1901@gmail.com>
2018-09-07 01:46:02 -0500

    add email exception class

--- lib/centaman/exceptions.rb
+++ lib/centaman/exceptions.rb
@@ -3,5 +3,6 @@ module Centaman
     class CentamanError < StandardError; end
     class CentamanTimeout < CentamanError; end
     class CentamanUnauthorized < CentamanError; end
+    class CentamanEmailError< CentamanError; end
   end
 end
